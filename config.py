b962b9b2a430fb4ecc5d07ad4c0d838ea8b3133c
Madara Shelby <140615675+MadaraShelby@users.noreply.github.com>
2023-11-03 08:53:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kokuofficalteam/Koku",
+    "https://github.com/kokuofficalteam/Koku.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
